496c01f9ee4dd3ca3782f9fa3e476f022345aca5
leungmanhin <manhinleung@gmail.com>
2019-04-02 16:50:01 +0800

    Start the RelEx server as well

--- notebook.sh
+++ notebook.sh
@@ -2,4 +2,8 @@
 
 source /home/relex/.profile
 source /home/relex/miniconda3/bin/activate demo
+
+# Start RelEx server
+/home/relex/relex-master/target/appassembler/bin/relexd &
+
 cd /home/relex/opencog-workshops-master/ && exec jupyter notebook --ip=0.0.0.0
